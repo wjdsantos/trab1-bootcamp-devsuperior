1577bfe04c8a30a9c1c2b3f232a5dd7e2bc30cc4
wjdsantos <walter4asantos@gmail.com>
2021-01-19 16:08:56 -0300

    Project created

---
+++ trab1bootcamp/src/main/java/com/devsuperior/trab1bootcamp/Trab1bootcampApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trab1bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trab1bootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trab1bootcampApplication.class, args);
+	}
+
+}
---
+++ trab1bootcamp/src/test/java/com/devsuperior/trab1bootcamp/Trab1bootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trab1bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trab1bootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
