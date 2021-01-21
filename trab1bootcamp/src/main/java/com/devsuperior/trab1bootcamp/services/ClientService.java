2e7a764e2e336cb91ea017a61594e9857eb0d0e6
wjdsantos <walter4asantos@gmail.com>
2021-01-21 14:47:23 -0300

    Delete client

--- trab1bootcamp/src/main/java/com/devsuperior/trab1bootcamp/services/ClientService.java
+++ trab1bootcamp/src/main/java/com/devsuperior/trab1bootcamp/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.trab1bootcamp.dto.ClientDTO;
 import com.devsuperior.trab1bootcamp.entities.Client;
 import com.devsuperior.trab1bootcamp.repositories.ClientRepository;
+import com.devsuperior.trab1bootcamp.services.exceptions.DatabaseException;
 import com.devsuperior.trab1bootcamp.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -60,6 +63,18 @@ public class ClientService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
